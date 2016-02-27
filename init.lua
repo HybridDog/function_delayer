9bdeb9b7c65bba4c737c2689a215a89148bb6181
HybridDog <ovvv@web.de>
2016-02-27 10:26:10 +0100

    add a table called tasks

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 -- abort if the function already exists
 if minetest.delay_function then
+	minetest.log("error", "[function_delayer] minetest.delay_function already exists.")
 	return
 end
 
@@ -9,9 +10,11 @@ local maxdelay = 1
 local skipstep = 5
 
 
+local tasks = {}
+
 -- used for the table.sort function
-local function sort_times(a,b)
-	return a[1] < b[1]
+local function sort_times(a, b)
+	return tasks[a][1] < tasks[b][1]
 end
 
 local needs_sort, toadd
@@ -20,7 +23,9 @@ function minetest.delay_function(time, func, ...)
 	if toadd then
 		time = time+toadd
 	end
-	todo[#todo+1] = {time, func, {...}}
+	local id = #tasks+1
+	todo[#todo+1] = id
+	tasks[id] = {time, func, {...}}
 
 	needs_sort = true
 end
@@ -57,19 +62,22 @@ minetest.register_globalstep(function(dtime)
 	toadd = dtime
 	local n = 1
 	while true do
-		if not todo[n] then
+		local id = todo[n]
+		if not id then
 			break
 		end
-		local time = todo[n][1]
+		local task = tasks[id]
+		local time = task[1]
 		time = time-dtime
 		if time < 0 then
-			local params = todo[n][3] or {}
+			local params = task[3] or {}
 			params[#params+1] = time
-			local func = todo[n][2]
+			local func = task[2]
 			table.remove(todo, n)
+			tasks[id] = nil
 			func(unpack(params))
 		else
-			todo[n][1] = time
+			task[1] = time
 			n = n+1
 		end
 		--print("expired")
@@ -79,9 +87,11 @@ minetest.register_globalstep(function(dtime)
 	-- execute functions until the time limit is reached
 	while todo[1]
 	and tonumber(os.clock())-ts < maxdelay do
-		local params = todo[1][3] or {}
-		params[#params+1] = todo[1][1]
-		local func = todo[1][2]
+		local task = tasks[todo[1]]
+		local params = task[3] or {}
+		params[#params+1] = task[1]
+		local func = task[2]
+		tasks[todo[1]] = nil
 		table.remove(todo, 1)
 		func(unpack(params))
 	end
